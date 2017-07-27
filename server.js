7859e43bd20e9cc64f7f37709a8dd409391c6aec
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2017-07-28 00:34:41 +0200

    minor refactoring

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+
 app.get('/', function(req, res){
   res.sendFile(`${__dirname}/src/index.html`);
 });
 
-app.use(express.static('src/'));
+app.use(express.static('src'));
 
 io.on('connection', socket => {
-  socket.on('chat message', function(msg) {
+  socket.on('chat message', msg => {
     console.log(msg);
     io.emit('chat message', msg);
   });
 });
 
-
-http.listen(1337, function(){
-  console.log('listening on *:1337');
+http.listen(1337, () => {
+  console.log('listening on *: 1337');
 });
 
 
--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 const socket = io();
 
-const formEL = document.querySelector('.js_form');
+const trigger = document.querySelector('.js_trigger');
 const inputEL = document.getElementById('m');
 const messagesEl = document.getElementById('messages');
 const li = document.createElement('li');
+let item = messagesEl.appendChild(li);
 
-
-formEL.addEventListener('submit', function(event) {
+trigger.addEventListener('click', function(event) {
   socket.emit('chat message', inputEL.value);
     inputEL.value = ' ';
     return false;
 });
+
 socket.on('chat message', function(msg) {
     console.log(msg);
-    messagesEl.appendChild(li).append(msg);
+    var text = document.createTextNode(msg);
+    item.appendChild(text);
 });
