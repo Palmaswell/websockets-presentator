3e03bf248a7c2d06f46d862c7aa4b91dda621338
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2017-09-08 20:33:55 +0200

    Include ambient light illuminance logic

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,21 @@
 function handleAmbientLight () {
   const sensor = new AmbientLightSensor();
+  const theme = document.querySelector('.withTheme');
   sensor.start();
 
-  sensor.onreading = e => console.log(e);
-};
-
+  sensor.onreading = e => {
+    console.log('e.currentTarget.activated', e.currentTarget.activated);
+    switch(e.currentTarget.activated) {
+      case e.currentTarget.illuminance <= 8:
+        console.log(e.currentTarget.illuminance <= 8, 'e.currentTarget.illuminance <= 8')
+        theme.style.setProperty('--theme-invert', '1');
+        break;
+      case e.currentTarget.illuminance >= 9:
+      default:
+        console.log(e.currentTarget.illuminance >= 9, 'e.currentTarget.illuminance >= 9')
+        theme.style.setProperty('--theme-invert', 'unset');
+        break;
+    }
+  }
+}
 handleAmbientLight();
