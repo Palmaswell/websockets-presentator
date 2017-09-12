fa5e6cc2c2601b5aaea5b5f9852adb7ce0d39b06
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2017-09-12 23:15:26 +0200

    Optimize confetti debounce function

--- src/confetti.js
+++ src/confetti.js
@@ -2,39 +2,51 @@ function confetti () {
   const socket = io();
   const root = document.querySelector(':root');
 
-  const handleMotion = e => {
-    const position = {
-      x: e.beta,
-      y: e.gamma,
-      ticking: false
-    }
-    handleTick(position);
-  }
-
-  const handleTick = (position) => {
-    if (!position.ticking) {
-      position.ticking = true;
-      requestAnimationFrame(() => {
-        update(position);
-      });
-    }
+  const position = {
+    x: 0,
+    y: 0,
   };
 
-  const update = (position) => {
-    socket.on('position', position => {
-      const maxDegX = position.x < 0 ? -180 : 180;
-      const maxDegY = position.y < 0 ? -90 : 90;
+  const written = {
+    x: 0,
+    y: 0
+  };
 
-      position.x = (position.x / maxDegX) * 2 - 1;
-      position.y = (position.y / maxDegY) * 2 - 1;
-
-      root.style.setProperty('--x', position.x);
-      root.style.setProperty('--y', position.y);
-
-      position.ticking = false;
-    });
-    socket.emit('position', position);
+  const handleMotion = e => {
+    position.x = Math.round(e.beta * 100) / 100;
+    position.y = Math.round(e.gamma * 100) / 100;
   }
+
+
+  const update = () => {
+    if (position.x !== written.x) {
+      const maxDegX = position.x < 0 ? -180 : 180;
+      written.x = position.x;
+
+      root.style.setProperty('--x', (position.x / maxDegX) * 2 - 1);
+    }
+
+    if (position.y !== written.y) {
+      const maxDegY = position.y < 0 ? -90 : 90;
+      written.y = position.y + .1;
+
+      root.style.setProperty('--y', (position.y / maxDegY) * 2 - 1);
+    }
+
+    if (position.x !== written.x
+      || position.y !== written.y) {
+      socket.emit('position', position);
+    }
+
+    requestAnimationFrame(update);
+  };
+
+  update();
+
+  socket.on('position', data => {
+    position.x = data.x;
+    position.y = data.y;
+  });
 
   window.addEventListener('deviceorientation',
     handleMotion, { capture: true,  passive: true }
