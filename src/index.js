e134514663197dde73efe3aed2576ab23f642549
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2017-09-26 19:15:18 +0200

    Include second example for the presentation

--- src/index.js
+++ src/index.js
@@ -1,61 +1,25 @@
-function trainedLion () {
-  const socket = io();
+function galaxy () {
   const root = document.querySelector(':root');
-
-  const orientation = {
-    x: 0,
-    y: 0
-  }
-
-  const written = {
-    x: 0,
-    y: 0
-  }
-
-  const handleMotion = e => {
-    orientation.x = Math.round(e.beta * 100) / 100;
-    orientation.y = Math.round(e.gamma * 100) / 100;
-  }
+  const motion = {
+    time: 0,
+    step: 0.01
+  };
+  const angle = {
+    sin: 0,
+    cos: 0
+  };
 
   const update = () => {
-    if (orientation.x !== written.x) {
-      const maxDegX = orientation.x < 0 ? -180 : 180;
-      written.x = orientation.x;
+    motion.time = (motion.time + 0.001) % 1;
+    angle.sin = Math.sin(motion.time * Math.PI * 2);
+    angle.cos = Math.cos(motion.time * Math.PI * 2);
 
-      root.style.setProperty('--y', (orientation.x / maxDegX) * 2 - 1);
-    }
-
-    if (orientation.y !== written.y) {
-      const maxDegY = orientation.y < 0 ? -90 : 90;
-
-      root.style.setProperty('--x', (orientation.y / maxDegY) * 2 - 1);
-    }
-
-    if (orientation.x !== written.x
-      || orientation.y !== written.y) {
-      socket.emit('orientation', orientation);
-    }
-
+    root.style.setProperty('--sin', angle.sin);
+    root.style.setProperty('--cos', angle.cos);
     requestAnimationFrame(update);
-  }
+  };
 
   update();
-
-  window.addEventListener('deviceorientation',
-    handleMotion, { capture: true,  passive: true }
-  );
-  socket.on('orientation', (data) => {
-    orientation.x = data.x;
-    orientation.y = data.y;
-  });
-
-  window.addEventListener('mousemove', e => {
-    const x = e.pageX / window.innerWidth * 2 - 1;
-    const y = e.pageY / window.innerHeight * 2 - 1;
-    root.style.setProperty('--x', x);
-    root.style.setProperty('--y', y);
-  },
-  { capture: true,  passive: true });
 };
 
-trainedLion();
+galaxy();
